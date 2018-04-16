b346ce8ac57c60406939e0fd5e14100befb8e538
JohanTerrenoire <terrenoirejohan@yahoo.fr>
2018-04-16 19:23:30 +0200

    Script d'import de données

--- tools/import_devices.py
+++ tools/import_devices.py
@@ -25,10 +25,62 @@ with erppeek.Client.from_config('test') as client:
     else:
         #-----Lire le fichier CSV-----
         with open('device.csv', 'rb') as csvfile:
-            device_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
-            for row in device_reader :
-                #-----Écrire chaque ligne dans la base de données-----
-                client.model('iut.it.device').create({'name': row[3], 'date_allocation': row[1], 'serial_number': row[0], 'date_purchase': row[2]})
+            fichier_csv = csv.reader(csvfile, delimiter=';', quotechar='|')
+            for row in fichier_csv:
+                pMarque = client.search('iut.it.brand', [('name', '=', row[4])])
+
+                pTypeModel = client.search('iut.it.model.type', [('name', '=', row[3])])
+
+                pModele = client.search('iut.it.model', [('name', '=', row[5])])
+                print ("Existe déjà")
+
+                pPartenaire = client.search('res.partner', [('name', '=', row[6])])
+
+                pNumeroSerie = client.search('iut.it.device', [('serial_number','=', row[0]), ('date_allocation','=', row[1]), ('date_purchase','=', row[2])])
+
+                print('')
+
+                #insertion des données dans la bdd si elles n'existent pas
+
+                #ajout des valeurs de la marque
+                if len(pMarque) == 0:
+                     vBrand = client.create('iut.it.brand', {'name':row[4], 'warranty_delay_month':12})
+                else:
+                    vBrand = pMarque[0]
+                    print('Erreur')
+
+                #ajout des valeurs du type de modèle
+                if len(pTypeModel) == 0:
+                     vModelType = client.create('iut.it.model.type', {'name': row[5]})
+                else:
+                    vModelType = pTypeModel[0]
+                    print('Erreur')
+
+                #ajout des valeurs du modèle
+                if len(pModele) == 0:
+                     vModel = client.create('iut.it.model', {'name': row[3], 'brand_id': vBrand})
+                else:
+                    vModel = pModele[0]
+                    print('Erreur')
+
+                #ajout des valeurs de l'employé
+                if len(pPartenaire) == 0:
+                     vPartner = client.create('res.partner', {'name': row[6]})
+                else:
+                    vPartner = pPartenaire[0]
+                    print('Erreur')
+
+                #ajout des valeurs du dispositif
+                if len(pNumeroSerie) == 0:
+                     vDevice = client.create('iut.it.device', {'name': 'xxx',
+                                                                'serial_number': row[0],
+                                                                'date_allocation': row[1],
+                                                                'date_purchase': row[2],
+                                                                'partner_id': vPartner,
+                                                                'model_id': vModel})
+                else:
+                    vDevice = pNumeroSerie[0]
+                    print('Erreur')
 
 
 # Insérer un enregistrement avec ERPPEEK :
